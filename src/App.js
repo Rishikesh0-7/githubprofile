c74a00f53e29edcbbb83697aef86681d2687829a
CodeWithRishi <68162560+CodeWithRishi@users.noreply.github.com>
2021-09-11 17:48:32 +0530

    Finish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import 'font-awesome/css/font-awesome.min.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+  async function getResults() {
+    const user = `https://api.github.com/users/${document.getElementById('name').value}`
+    let response = await fetch(user)
+    const url = ""
+    const json_data = await response.json()
+    console.log(json_data)
+    
+    document.getElementById("uname").innerHTML = `${json_data.login}`
+    document.getElementById("fo").innerHTML = `${json_data.followers}`
+    document.getElementById("foo").innerHTML = `${json_data.following}`
+    document.getElementById("url").href = `${json_data.url}`
+    document.getElementById("web").innerHTML = `${json_data.blog}`
+  }
+
+  return <>
+    <div>
+      <section>
+        <div className="inputs">
+          <input id="name" type="text" placeholder="Enter Username" />
+          <button onClick={getResults}  className="button">Go</button>
+        </div>
+        <div className="results">
+          <p><i className="fa fa-user" style={{color : "rgb(30, 163, 30)" , fontSize : 20 , paddingRight : 5}}></i> Name : <span id="uname">Unknown</span></p>
+        </div>
+        <div className="results">
+        
+          <p><i className="fa fa-user-plus" style={{ color: "rgb(30, 163, 30)", fontSize: 20}}></i> Followers : <span id="fo">0</span></p>
+        </div>
+        <div className="results">
+        
+          <p><i className="fa fa-male" style={{ color: "rgb(30, 163, 30)", fontSize: 20 , paddingRight : 5}}></i> Following : <span id="foo">0</span></p>
+        </div>
+        <div className="results">
+        
+          <p><i className="fa fa-link" style={{ color: "rgb(30, 163, 30)", fontSize: 20 , paddingRight : 5}}></i> URL : <a href="" id="url">Click Here!</a></p>
+        </div>
+        <div className="results">
+        
+          <p><i className="fa fa-globe" style={{ color: "rgb(30, 163, 30)", fontSize: 20 , paddingRight : 5}}></i> Website : <span id="web">No website.</span></p>
+        </div>
+      </section>
     </div>
-  );
+  </>;
 }
 
 export default App;
